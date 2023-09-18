855601261e64ba8e744fca9371f11e1bec3681dd
Marjorie Kohn <marjoriekohn@icloud.com>
2023-09-17 20:59:16 -0400

    Update quoteGenerator url - netlify
    Signed-off-by: Marjorie Kohn <marjoriekohn@icloud.com>

--- src/Cards.js
+++ src/Cards.js
@@ -1,12 +1,12 @@
 import {ProjectCard} from "./ProjectCard.js";
 
 export function generateCardContent() {
-		// project Quote Me
-		const projectQuoteGenerator = new ProjectCard("Quote Generator", "A dynamic quote generator that leverages API" +	" Ninjas Quotes API.", ["HTML", "CSS", "Javascript", "NodeJS"], "https://github.com/marjoriekohn/quote-generator", "https://marjoriekohn.github.io/quote-generator/");
+		// project Quote Generator
+		const projectQuoteGenerator = new ProjectCard("Quote Generator", "A dynamic quote generator that leverages API" +	" Ninjas Quotes API.", ["HTML", "CSS", "Javascript", "NodeJS"], "https://github.com/marjoriekohn/quote-generator", "https://main--marjorie-kohn-quote-generator.netlify.app");
 		const projectCardQuoteGenerator = projectQuoteGenerator.createCardElement();
 		document.querySelector(".grid-container").appendChild(projectCardQuoteGenerator);
 
-		// project What the Func!
+		// project Javascript Functions
 		const projectJSFunctions = new ProjectCard("JavaScript Functions", "A repository of versatile JavaScript" +	" functions.", ["HTML", "CSS", "Javascript"], "https://github.com/marjoriekohn/javascript-functions", "https://marjoriekohn.github.io/javascript-functions/");
 		const projectCardJSFunctions = projectJSFunctions.createCardElement();
 		document.querySelector(".grid-container").appendChild(projectCardJSFunctions);
@@ -16,7 +16,7 @@ export function generateCardContent() {
 		const projectCardByteWeather = projectByteWeather.createCardElement();
 		document.querySelector(".grid-container").appendChild(projectCardByteWeather);
 
-		// project jQulator
+		// project Calculator
 		const projectCalculator = new ProjectCard("Calculator", "A simple calculator built using jQuery.", ["HTML", "CSS", "Javascript"], "https://github.com/marjoriekohn/calculator", "https://marjoriekohn.github.io/calculator/");
 		const projectCardCalculator = projectCalculator.createCardElement();
 		document.querySelector(".grid-container").appendChild(projectCardCalculator);
