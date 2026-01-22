11510b95911cf8d5a1d63ec27abf158eb4070273
Marjorie Kohn <marjoriekohn@icloud.com>
2026-01-22 10:55:06 -0500

    Update project cards in Cards.js
    Added 'Public Dataset Analysis' project card. Commented out the 'Weather' project card.
    
    Signed-off-by: Marjorie Kohn <marjoriekohn@icloud.com>

--- src/Cards.js
+++ src/Cards.js
@@ -2,11 +2,6 @@ import {ProjectCard} from "./ProjectCard.js";
 
 export function generateCardContent() {
 
-		// project Ask AI
-		const projectBNFO620 = new ProjectCard("Ask AI", "An asynchronous python program to query AI models.", ["Python", "R"], "https://github.com/marjoriekohn/bnfo620_ai_caller", "https://github.com/marjoriekohn/bnfo620_ai_caller");
-		const projectCardBNFO620 = projectBNFO620.createCardElement();
-		document.querySelector(".grid-container").appendChild(projectCardBNFO620);
-
 		// project Rosalind
 		const projectRosalind = new ProjectCard("Rosalind", "A repository of solutions to bioinformatics coding challenges.", ["Python"], "https://github.com/marjoriekohn/rosalind", "https://marjoriekohn-rosalind.streamlit.app/");
 		const projectCardRosalind = projectRosalind.createCardElement();
@@ -17,18 +12,28 @@ export function generateCardContent() {
 		const projectCardJSFunctions = projectJSFunctions.createCardElement();
 		document.querySelector(".grid-container").appendChild(projectCardJSFunctions);
 
+		// project Ask AI
+		const projectBNFO620 = new ProjectCard("Ask AI", "An asynchronous python program to query AI models.", ["Python"], "https://github.com/marjoriekohn/bnfo620_ai_caller", "https://github.com/marjoriekohn/bnfo620_ai_caller");
+		const projectCardBNFO620 = projectBNFO620.createCardElement();
+		document.querySelector(".grid-container").appendChild(projectCardBNFO620);
+
+		// project Public Dataset Analysis
+		const projectDatasetAnalysis = new ProjectCard("Public Dataset Analysis", "An analysis of the CDCs Chronic Disease Indicators dataset.", ["R"], "https://github.com/marjoriekohn/Chronic-Disease-Indicators---Analysis", "https://github.com/marjoriekohn/Chronic-Disease-Indicators---Analysis");
+		const projectCardDatasetAnalysis = projectDatasetAnalysis.createCardElement();
+		document.querySelector(".grid-container").appendChild(projectCardDatasetAnalysis);
+
+		// project Quote Generator
+		const projectQuoteGenerator = new ProjectCard("Quotes", "Fetches random quotes from an external API.", ["HTML", "CSS", "JavaScript", "NodeJS"], "https://github.com/marjoriekohn/quote-generator", "https://quotes.marjoriekohn.com");
+		const projectCardQuoteGenerator = projectQuoteGenerator.createCardElement();
+		document.querySelector(".grid-container").appendChild(projectCardQuoteGenerator);
+	
 		// project Calculator
 		const projectCalculator = new ProjectCard("Calculator", "A simple calculator built using jQuery.", ["HTML", "CSS", "Javascript"], "https://github.com/marjoriekohn/calculator", "https://calculator.marjoriekohn.com");
 		const projectCardCalculator = projectCalculator.createCardElement();
 		document.querySelector(".grid-container").appendChild(projectCardCalculator);
 	
-		// project Quote Generator
-		const projectQuoteGenerator = new ProjectCard("Quotes", "Fetches random quotes from an external API.", ["HTML", "CSS", "JavaScript", "NodeJS"], "https://github.com/marjoriekohn/quote-generator", "https://quotes.marjoriekohn.com");
-		const projectCardQuoteGenerator = projectQuoteGenerator.createCardElement();
-		document.querySelector(".grid-container").appendChild(projectCardQuoteGenerator);
-
 		// project Weather
-		const projectByteWeather = new ProjectCard("Weather", "A real-time weather data platform that uses OpenWeather API.", ["HTML", "CSS", "Javascript", "NodeJS"], "https://github.com/marjoriekohn/weather", "https://weather.marjoriekohn.com");
-		const projectCardByteWeather = projectByteWeather.createCardElement();
-		document.querySelector(".grid-container").appendChild(projectCardByteWeather);
+		// const projectByteWeather = new ProjectCard("Weather", "A real-time weather data platform that uses OpenWeather API.", ["HTML", "CSS", "Javascript", "NodeJS"], "https://github.com/marjoriekohn/weather", "https://weather.marjoriekohn.com");
+		// const projectCardByteWeather = projectByteWeather.createCardElement();
+		// document.querySelector(".grid-container").appendChild(projectCardByteWeather);
 }
